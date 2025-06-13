44faa379fee991a777f1607fc0e410617f087ef6
ZM <123998314+Zxrri@users.noreply.github.com>
2025-06-13 09:49:29 -0400

    Added Audio Controller

--- MuteMe/MuteMe/Services/AudioController.cs
+++ MuteMe/MuteMe/Services/AudioController.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace MuteMe.Services
 {
-    internal class AudioController
+    using NAudio.CoreAudioApi;
+
+    public class AudioController
     {
+        private static readonly MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+
+        public static void MuteSystemAudio(bool mute)
+        {
+            var device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            device.AudioEndpointVolume.Mute = mute;
+        }
+
+        public static void MuteMic(bool mute)
+        {
+            var mic = enumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Multimedia);
+            mic.AudioEndpointVolume.Mute = mute;
+        }
     }
+
 }
