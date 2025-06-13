f9bced50f135cf69ce929b05c718c01afcc5a7cd
ZM <123998314+Zxrri@users.noreply.github.com>
2025-06-13 09:48:44 -0400

    Basic Idle Detection Logic done

--- MuteMe/MuteMe/Services/IdleWatcher.cs
+++ MuteMe/MuteMe/Services/IdleWatcher.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace MuteMe.Services
 {
-    internal class IdleWatcher
+    using System.Runtime.InteropServices;
+
+    public class IdleWatcher
     {
+        [DllImport("user32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct LASTINPUTINFO
+        {
+            public uint cbSize;
+            public uint dwTime;
+        }
+
+        public static int GetIdleTimeMs()
+        {
+            LASTINPUTINFO lastInput = new LASTINPUTINFO();
+            lastInput.cbSize = (uint)Marshal.SizeOf(lastInput);
+            GetLastInputInfo(ref lastInput);
+
+            return Environment.TickCount - (int)lastInput.dwTime;
+        }
     }
+
 }
